6036b8f3fb6684b04b2986c0eb323b307524b07b
Marypaz Navarrete <maripaznavarrete@hotmail.com>
2018-04-24 21:39:51 -0500

    Correlacion

---
+++ correlacion.R
@@ -0,0 +1,26 @@
+setwd("C:/Users/marip_000/Desktop/specdata")
+directorio <- "C:/Users/marip_000/Desktop/specdata"
+
+corr <- function(directorio,horizonte=0){
+    vec1 <- vertor("numeric")
+    vec2 <- vector("numeric")
+    
+    for(i in 1:332){
+        if(i <10){
+            i = paste("00",i,sep = "")
+        } else if (i>=10 && i<100){
+            i = paste("0",i,sep = "")
+        } else {
+            i=paste(i,sep = "")
+        }
+        leer <- read.csv(paste(i,".csv",sep = ""))
+        datos <- complete.cases(leer)
+        reales  <- leer[datos,2:3]
+        numero <- nrow(reales)
+        if(numero >= horizonte){
+            vec1 <- cor(reales[,1],reales[,2])
+            vec2 <- c(vec2,vec1)
+        }
+    }
+    vec2
+}
